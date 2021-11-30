907f676f25b2b0fe4218b2aa48a34377d17ec511
shoebkhan4256 <shoebkhan4256@gmail.com>
2021-11-30 13:01:50 +0530

    update

---
+++ pdac3/src/App.js
@@ -0,0 +1,20 @@
+import {Navigation} from "./Component.jsx/Navigation"
+
+function App() {
+  
+    <div className="App">
+    <Navigation/>
+<div>
+{""}
+  <h1>Navigation</h1>
+  <nav>
+    <a ></a>
+  </nav>
+</div>
+
+    
+    </div>
+  );
+}
+
+export default App;
